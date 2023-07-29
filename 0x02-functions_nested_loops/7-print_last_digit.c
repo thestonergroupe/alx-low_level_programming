449dc2c981dbe56b1f8fdff5bb90f82bfc09d940
abderrahmane <abderrahmane.elmrini@gmail.com>
2023-07-29 05:30:10 -0700

    jqs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  *  * print_last_digit - prints the last digit of a number
  *   * @n: the int to extract the last digit from
@@ -21,5 +21,4 @@ int print_last_digit(int n)
 							return (a);
 									
 }
-
 
